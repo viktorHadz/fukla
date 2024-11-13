c5aa69ae16f92b4a9b3433b5481add93adc02f41
drslowdeath <vikecah@gmail.com>
2024-11-13 00:26:10 +0000

    Added Tailwind breakpoint & responsive homepage.

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,48 +1,64 @@
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="hero__text">
-        <div className="hero__box">
-          <h2 className="hero__text__title">Контакти</h2>
-          <div className="hero__description__box">
-            <a className="hero__text__desc" href="tel:+359884218445">
-              T:+359 884 218 445
-            </a>
-            <a
-              className="hero__text__desc"
-              href="mailto:contact@slavinaradanova.com"
-            >
-              contact@slavinaradanova.com
-            </a>
+      <div className="w-full px-4 py-8 text-white bg-gray-900 md:px-6 apply-bg">
+        <div className="grid grid-cols-1 gap-8 text-center sm:grid-cols-2 md:grid-cols-3">
+          {/* Contact Section */}
+          <div className="flex flex-col items-center">
+            <h2 className="mb-4 text-2xl font-semibold underline-txt">
+              Контакти
+            </h2>
+            <div className="space-y-2">
+              <a
+                className="block cursor-pointer hover:underline"
+                href="tel:+359884218445"
+              >
+                T:+359 884 218 445
+              </a>
+              <a
+                className="block cursor-pointer hover:underline"
+                href="mailto:contact@slavinaradanova.com"
+              >
+                contact@slavinaradanova.com
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="hero__box">
-          <h2 className="hero__text__title">Работно време</h2>
-          <div className="hero__description__box">
-            <p className="hero__text__desc">Пон до Пет: 09:00ч. - 18:00ч.</p>
-            <p className="hero__text__desc">Събота: 10:00ч. - 15:00ч.</p>
-            <p className="hero__text__desc">Неделя: почивен</p>
+
+          {/* Working Hours Section - Hidden below `sm` breakpoint */}
+          <div className="flex-col items-center hidden md:flex">
+            <h2 className="mb-4 text-2xl font-semibold underline-txt">
+              Работно време
+            </h2>
+            <div className="space-y-2">
+              <p>Пон до Пет: 09:00ч. - 18:00ч.</p>
+              <p>Събота: 10:00ч. - 15:00ч.</p>
+              <p>Неделя: почивен</p>
+            </div>
           </div>
-        </div>
-        <div className="hero__box">
-          <h2 className="hero__text__title">Локация</h2>
-          <div className="hero__description__box">
-            <a
-              className="hero__text__desc"
-              href="https://goo.gl/maps/JyPuf91ztik3HE1r8"
-            >
-              Велико Търново
-            </a>
-            <a
-              className="hero__text__desc"
-              href="https://goo.gl/maps/JyPuf91ztik3HE1r8"
-            >
-              Пл. Пенчо Славейков 4
-            </a>
+
+          {/* Location Section */}
+          <div className="flex flex-col items-center">
+            <h2 className="mb-4 text-2xl font-semibold underline-txt">
+              Локация
+            </h2>
+            <div className="space-y-2">
+              <a
+                className="block cursor-pointer hover:underline"
+                href="https://goo.gl/maps/JyPuf91ztik3HE1r8"
+              >
+                Велико Търново
+              </a>
+              <a
+                className="block cursor-pointer hover:underline"
+                href="https://goo.gl/maps/JyPuf91ztik3HE1r8"
+              >
+                Пл. Пенчо Славейков 4
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="w-full">
         <div className="copyright">
           &copy; Copyright <strong>Fukla</strong>. All Rights Reserved
         </div>
--- src/components/pages/home/Services.js
+++ src/components/pages/home/Services.js
@@ -2,31 +2,63 @@ import { Link } from "react-router-dom";
 export const Services = () => {
   return (
     <>
-      <section className="w-full apply-bg text-white py-12 my-20">
-        <div className="flex flex-col md:flex-row items-center md:items-start max-w-7xl mx-auto px-6 sm:px-20 2xl:px-0 2xl:gap-16">
+      <section className="w-full py-12 my-20 text-white apply-bg">
+        <div className="flex flex-col items-center px-6 mx-auto md:flex-row md:items-start max-w-7xl sm:px-20 2xl:px-0 2xl:gap-16">
           <div className="w-full md:w-1/3">
             <img
-              className="w-2/3 sm:w-3/4 md:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
+              className="w-2/3 xs:w-3/4 sm:w-3/4 md:w-full lg:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
               src="/images/home/braids.webp"
               alt="Braids"
             />
           </div>
-          <div className="w-3/4 md:w-2/3 md:pl-10 mt-6 md:mt-0 mx-auto">
-            <h2 className="text-2xl md:text-3xl font-semibold underline-txt mb-4 md:mb-6">
+          <div className="w-3/4 mx-auto mt-6 md:w-2/3 md:pl-10 md:mt-0">
+            <h2 className="mb-2 text-2xl font-semibold md:text-3xl underline-txt md:mb-4">
               Плитки
             </h2>
-            <p className="text-sm md:text-base lg:text-lg mb-4 md:mb-6 leading-relaxed">
+            <p className="mb-4 text-sm leading-relaxed md:text-base lg:text-lg md:mb-6">
               Косата е короната, която никога не сваляте и с правилната грижа и
               оформяне тя може да бъде блестящо свидетелство за вашата красота и
               увереност.
             </p>
-            <p className="text-sm md:text-base lg:text-lg mb-6 md:mb-8 leading-relaxed">
+            <p className="hidden mb-6 text-sm leading-relaxed sm:flex md:text-base lg:text-lg md:mb-8">
               Косата е отражение на това кой сте и какво представлявате, а с
               правилния стил тя може да бъде мощен израз на вашата идентичност.
             </p>
             <Link
               to="/braids"
-              className="inline-block site-button first-letter:capitalize text-sm md:text-base"
+              className="inline-block text-sm site-button first-letter:capitalize md:text-base"
+            >
+              прочети повече
+            </Link>
+          </div>
+        </div>
+      </section>
+      {/* MARK: Hair Thread */}
+      <section className="w-full py-12 my-20 text-white apply-bg">
+        <div className="flex flex-col items-center px-6 mx-auto md:flex-row md:items-start max-w-7xl sm:px-20 2xl:px-0 2xl:gap-16">
+          <div className="w-full md:w-1/3">
+            <img
+              className="w-2/3 xs:w-3/4 sm:w-3/4 md:w-full lg:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
+              src=""
+              alt="Hair Extensions"
+            />
+          </div>
+          <div className="w-3/4 mx-auto mt-6 md:w-2/3 md:pl-10 md:mt-0">
+            <h2 className="mb-2 text-2xl font-semibold md:text-3xl underline-txt md:mb-4">
+              Прежда за Плитки
+            </h2>
+            <p className="mb-4 text-sm leading-relaxed md:text-base lg:text-lg md:mb-6">
+              Преждата за плитки е идеалният начин да внесете цвят и текстура
+              във вашата прическа, добавяйки нови измерения на вашия стил.
+            </p>
+            <p className="hidden mb-6 text-sm leading-relaxed sm:flex md:text-base lg:text-lg md:mb-8">
+              Тази техника е идеален избор за онези, които искат да изразят
+              своята индивидуалност с ярък и впечатляващ акцент.
+            </p>
+            {/* MARK: TODO New page needed */}
+            <Link
+              to="/nails"
+              className="inline-block text-sm site-button first-letter:capitalize md:text-base"
             >
               прочети повече
             </Link>
@@ -34,62 +66,97 @@ export const Services = () => {
         </div>
       </section>
 
-      <section className="w-full apply-bg text-white py-12 my-20">
-        <div className="flex flex-col md:flex-row-reverse items-center md:items-start max-w-7xl mx-auto px-6 sm:px-20 2xl:px-0 2xl:gap-16">
+      {/* MARK:Temporary Tatoos */}
+      <section className="w-full py-12 my-20 text-white apply-bg">
+        <div className="flex flex-col items-center px-6 mx-auto md:flex-row-reverse md:items-start max-w-7xl sm:px-20 2xl:px-0 2xl:gap-16">
           <div className="w-full md:w-1/3">
             <img
-              className="w-2/3 sm:w-3/4 md:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
+              className="w-2/3 xs:w-3/4 sm:w-3/4 md:w-full lg:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
+              src="/"
+              alt="Temporary Tatoos"
+            />
+          </div>
+          <div className="w-3/4 mx-auto mt-6 md:w-2/3 md:pr-10 md:mt-0">
+            <h2 className="mb-2 text-2xl font-semibold md:text-3xl underline-txt md:mb-4">
+              Временни Татуировки
+            </h2>
+            <p className="mb-4 text-sm leading-relaxed md:text-base lg:text-lg md:mb-6">
+              Временните татуировки са начин да изразите своята креативност и
+              уникалност, без да правите постоянен ангажимент.
+            </p>
+            <p className="hidden mb-6 text-sm leading-relaxed sm:flex md:text-base lg:text-lg md:mb-8">
+              От смели и привличащи вниманието до деликатни и едва
+              забележими.Тази форма на изкуство е перфектен избор за тези, които
+              обичат да експериментират и искат да изпробват нови идеи за
+              самоизразяване.
+            </p>
+            {/* MARK: TODO: New page needed */}
+            <Link
+              to="/makeup"
+              className="inline-block text-sm site-button first-letter:capitalize md:text-base"
+            >
+              виж още
+            </Link>
+          </div>
+        </div>
+      </section>
+      {/* MARK: Makeup - REMOVE*/}
+      <section className="w-full py-12 my-20 text-white apply-bg">
+        <div className="flex flex-col items-center px-6 mx-auto md:flex-row-reverse md:items-start max-w-7xl sm:px-20 2xl:px-0 2xl:gap-16">
+          <div className="w-full md:w-1/3">
+            <img
+              className="w-2/3 xs:w-3/4 sm:w-3/4 md:w-full lg:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
               src="/images/home/makeup.webp"
               alt="Makeup"
             />
           </div>
-          <div className="w-3/4 md:w-2/3 md:pr-10 mt-6 md:mt-0 mx-auto">
-            <h2 className="text-2xl md:text-3xl font-semibold underline-txt mb-4 md:mb-6">
+          <div className="w-3/4 mx-auto mt-6 md:w-2/3 md:pr-10 md:mt-0">
+            <h2 className="mb-2 text-2xl font-semibold md:text-3xl underline-txt md:mb-4">
               Грим
             </h2>
-            <p className="text-sm md:text-base lg:text-lg mb-4 md:mb-6 leading-relaxed">
+            <p className="mb-4 text-sm leading-relaxed md:text-base lg:text-lg md:mb-6">
               Гримът не е маска, която прикрива вашата красота, това е оръжие,
               което ви помага да изразите коя сте отвътре.
             </p>
-            <p className="text-sm md:text-base lg:text-lg mb-6 md:mb-8 leading-relaxed">
+            <p className="hidden mb-6 text-sm leading-relaxed sm:flex md:text-base lg:text-lg md:mb-8">
               Красотата на грима е, че може да се използва за подчертаване на
               естествената красота, която вече съществува, вместо да се опитва
               да я прикрие или промени напълно.
             </p>
             <Link
-              to=""
-              className="inline-block site-button first-letter:capitalize text-sm md:text-base"
+              to="/makeup"
+              className="inline-block text-sm site-button first-letter:capitalize md:text-base"
             >
               търси се
             </Link>
           </div>
         </div>
       </section>
-
-      <section className="w-full apply-bg text-white py-12 my-20">
-        <div className="flex flex-col md:flex-row items-center md:items-start max-w-7xl mx-auto px-6 sm:px-20 2xl:px-0 2xl:gap-16">
+      {/* MARK: Nails */}
+      <section className="w-full py-12 my-20 text-white apply-bg">
+        <div className="flex flex-col items-center px-6 mx-auto md:flex-row md:items-start max-w-7xl sm:px-20 2xl:px-0 2xl:gap-16">
           <div className="w-full md:w-1/3">
             <img
-              className="w-2/3 sm:w-3/4 md:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
+              className="w-2/3 xs:w-3/4 sm:w-3/4 md:w-full lg:w-5/6 h-auto object-cover rounded-md shadow-lg shadow-[#E95D7A] mx-auto"
               src="/images/home/nails.webp"
               alt="Nails"
             />
           </div>
-          <div className="w-3/4 md:w-2/3 md:pl-10 mt-6 md:mt-0 mx-auto">
-            <h2 className="text-2xl md:text-3xl font-semibold underline-txt mb-4 md:mb-6">
+          <div className="w-3/4 mx-auto mt-6 md:w-2/3 md:pl-10 md:mt-0">
+            <h2 className="mb-2 text-2xl font-semibold md:text-3xl underline-txt md:mb-4">
               Маникюр
             </h2>
-            <p className="text-sm md:text-base lg:text-lg mb-4 md:mb-6 leading-relaxed">
+            <p className="mb-4 text-sm leading-relaxed md:text-base lg:text-lg md:mb-6">
               Маникюрът е като бижу за вашите ръце, той добавя блясък и красота
               към вашия външен вид.
             </p>
-            <p className="text-sm md:text-base lg:text-lg mb-6 md:mb-8 leading-relaxed">
+            <p className="hidden mb-6 text-sm leading-relaxed sm:flex md:text-base lg:text-lg md:mb-8">
               Маникюрът е форма на изкуство и с правилните инструменти и техники
               можете да създавате красиви и уникални дизайни.
             </p>
             <Link
               to="/nails"
-              className="inline-block site-button first-letter:capitalize text-sm md:text-base"
+              className="inline-block text-sm site-button first-letter:capitalize md:text-base"
             >
               прочети повече
             </Link>

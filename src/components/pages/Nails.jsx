910867a41b343622c6a566c738a82f12ce845b72
drslowdeath <vikecah@gmail.com>
2024-12-02 23:25:51 +0000

    Small edits and text reworks

--- src/components/pages/Nails.jsx
+++ src/components/pages/Nails.jsx
@@ -2,6 +2,7 @@ import { NailsBanner } from './nails/NailsBanner';
 import { NailsHero1 } from './nails/NailsHero1';
 import { NailsStyles } from './nails/NailsStyles';
 import { NailsAboutMe } from './nails/NailsAboutMe';
+import { GalInsta } from './nails/galInsta';
 import { FAQ } from './nails/NailsFAQ';
 
 import ScrollToTopOnMount from '../ScrollToTopOnMount';
@@ -14,6 +15,7 @@ export const Nails = () => {
       <NailsHero1 />
       <NailsStyles />
       <NailsAboutMe />
+      <GalInsta />
       <FAQ />
     </>
   );
---
+++ src/components/pages/nails/galInsta.jsx
@@ -0,0 +1,17 @@
+export const GalInsta = () => {
+  return (
+    <section className='instagram'>
+      <h2 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+        Instagram
+      </h2>
+      <a
+        className='transition duration-100 rounded-md instagram__button'
+        target='_blank'
+        rel='noreferrer'
+        href='https://www.instagram.com/galiana.nails/'
+      >
+        Последвайте на @galiana.nails
+      </a>
+    </section>
+  );
+};
